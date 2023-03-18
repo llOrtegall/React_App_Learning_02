3d20df1c6f0205210ca392988988fe34dcfa8c2b
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-17 20:31:12 -0500

    hehehehee

--- src/context/Cart.jsx
+++ src/context/Cart.jsx
@@ -5,28 +5,20 @@ export const CartContext = createContext()
 
 function useCartReducer() {
   const [state, dispatch] = useReducer(cartReducer, cartInitialState)
-
   const addToCart = product => dispatch({
     type: 'ADD_TO_CART',
     payload: product
   })
-
   const removeFromCart = product => dispatch({
     type: 'REMOVE_FROM_CART',
     payload: product
   })
-
   const clearCart = () => dispatch({ type: 'CLEAR_CART' })
-
   return { state, addToCart, removeFromCart, clearCart }
 }
 
-// la dependencia de usar React Context
-// es MÍNIMA
-// eslint-disable-next-line react/prop-types
 export function CartProvider({ children }) {
   const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
-
   return (
     <CartContext.Provider value={{
       cart: state,
