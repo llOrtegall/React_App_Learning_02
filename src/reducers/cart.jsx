5e3478d9debbb7cbdad3be8179e17cf7c34ff35d
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-18 20:34:01 -0500

    marzo 18

--- src/reducers/cart.jsx
+++ src/reducers/cart.jsx
@@ -6,7 +6,6 @@ export const CART_ACTION_TYPES = {
   CLEAR_CART: 'CLEAR_CART'
 }
 
-// update localStorage with state for cart
 export const updateLocalStorage = state => {
   window.localStorage.setItem('cart', JSON.stringify(state))
 }
@@ -17,33 +16,14 @@ const UPDATE_STATE_BY_ACTION = {
     const productInCartIndex = state.findIndex(item => item.id === id)
 
     if (productInCartIndex >= 0) {
-      // 👀 una forma sería usando structuredClone
-      // const newState = structuredClone(state)
-      // newState[productInCartIndex].quantity += 1
-
-      // 👶 usando el map
-      // const newState = state.map(item => {
-      //   if (item.id === id) {
-      //     return {
-      //       ...item,
-      //       quantity: item.quantity + 1
-      //     }
-      //   }
-
-      //   return item
-      // })
-
-      // ⚡ usando el spread operator y slice
       const newState = [
         ...state.slice(0, productInCartIndex),
         { ...state[productInCartIndex], quantity: state[productInCartIndex].quantity + 1 },
         ...state.slice(productInCartIndex + 1)
       ]
-
       updateLocalStorage(newState)
       return newState
     }
-
     const newState = [
       ...state,
       {
@@ -51,7 +31,6 @@ const UPDATE_STATE_BY_ACTION = {
         quantity: 1
       }
     ]
-
     updateLocalStorage(newState)
     return newState
   },
@@ -66,7 +45,6 @@ const UPDATE_STATE_BY_ACTION = {
     return []
   }
 }
-
 export const cartReducer = (state, action) => {
   const { type: actionType } = action
   const updateState = UPDATE_STATE_BY_ACTION[actionType]
