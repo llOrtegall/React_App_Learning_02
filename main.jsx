e6d01f432cca029d6cdbb98665f4988d7ac29a40
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-13 12:05:54 -0500

    aplicando cambios

--- main.jsx
+++ main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import App from './src/App.jsx'
 // TODO: recordar importar y cambiar las extesiones de js a jsx para evitar error
 
 const root = createRoot(document.getElementById("app"));
 
-root.render(<App/>);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
